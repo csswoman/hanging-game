9ba2082b140c445f3d42b91606d2df7d628b6e3a
Karla Agraz <karla.agraz@gmail.com>
2022-10-26 11:32:31 -0500

    ADD: lógica para la palabra oculta y mostrar letras correctas en pantalla

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,50 @@
+import { useState } from 'react'
 import { letters } from './helpers/letters'
 import { HangImage } from './components/HangImage'
 import './App.css'
 
-
 function App() {
+  
+  const [ word ]= useState('COMPUTADORA');
+  const [ hiddenWord, setHiddenWord ] = useState( '_ '.repeat( word.length ) );
+  const [ attemtps, setAttempts ] = useState(0);
+
+  const checkLetter = ( letter: string ) => {
+
+    if ( !word.includes(letter) ) {
+      setAttempts ( Math.min ( attemtps + 1, 9 ) );
+      return;
+    }
+    
+    const hiddenWordArray = hiddenWord.split(' ');
+
+    for( let i=0; i < word.length; i++) {
+      if ( word[i] === letter ) {
+        hiddenWordArray[i] = letter;
+      }
+    }
+
+    setHiddenWord( hiddenWordArray.join(' ') );
+  }
+
   return (
     <div className="App">
       <h1>Hanging game</h1>
-        <HangImage imageNumber={ 9 } />
-    <h3>_ _ _ _ _ _</h3>
-      {
-        letters.map((letter) => (
-          <button key={ letter }>
-            { letter }
-            </button>
-        ))
-      }
 
+      <HangImage imageNumber={ attemtps } />
+      <h2>{ hiddenWord }</h2>
+
+      <h3>Intentos: { attemtps }</h3>
+        {
+          letters.map((letter) => (
+            <button
+              onClick={ () => checkLetter(letter) }
+              key={ letter }
+            >
+              { letter }
+              </button>
+          ))
+        }
     </div>
   )
 }
